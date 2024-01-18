576b87be5343b0a3d60d90d1063507fc024ef401
AnjasFedo <fedoaf98@gmail.com>
2024-01-18 14:15:32 +0700

    Create root and hello Handler

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"log"
 	"net/http"
-	"html"
 )
 
 func main() {
+	http.HandleFunc("/", rootHandler)
 
+	http.HandleFunc("/hello", helloHandler)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
 }
